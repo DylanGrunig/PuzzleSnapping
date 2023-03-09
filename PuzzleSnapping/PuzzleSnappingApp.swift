0efbced87fe5ddcc44da2a1dbee72e9daa6131be
DylanGrunig <Dylan.G24@k12.xrds.org>
2023-03-08 20:15:13 -0800

    Initial Commit

---
+++ PuzzleSnapping/PuzzleSnappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleSnappingApp.swift
+//  PuzzleSnapping
+//
+//  Created by Dylan Grunig on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleSnappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
